72075cbf7c570dec22a75ef0e6658e838a17ec93
bluecodeland <mhtaeb@gmail.com>
2021-02-12 19:12:26 +0330

    some changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,13 @@
     <div class="flex justify-center">
         <div class="sm:w-8/12 bg-white p-6 rounded-lg mt-10">
             
+  
             <div class="text-center p-5">
             welcome to Posty By Me
         </div>
         </div>
-
+        
     </div>
+    
 @endsection
 
